92892a1c1250c040d84087b57db6ae12d81b3311
= <suhaib.zuberi2@gmail.com>
2025-08-12 23:21:40 +0800

    Added some content on landing page and made dark mode transition more seemless by adjusting background image

--- src/Homepage/Home.tsx
+++ src/Homepage/Home.tsx
@@ -3,7 +3,13 @@ import "./Home.css";
 function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <div id="landing">
+        <h1 id="landing-title">AHZ Professionals</h1>
+        <p id="landing-caption">
+          Your Partner in Professional
+          <br /> Excellence
+        </p>
+      </div>
       <div id="placeholder">Hello, World!</div>
     </>
   );
